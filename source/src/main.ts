9904235943e4649a5820be2846f6fe76269aad88
SaSoup-YR <yuruiwang77@gmail.com>
2026-08-21 10:32:27 +0100

    Load RF-06 participant speech lifecycle repair

--- source/src/main.ts
+++ source/src/main.ts
@@ -1,5 +1,6 @@
 import './styles.css';
 import './accessible-nasa-tlx';
+import './rf06-speech-lifecycle';
 import { installRf04SavedSessionRecovery } from './rf04-saved-session-recovery';
 
 installRf04SavedSessionRecovery();
