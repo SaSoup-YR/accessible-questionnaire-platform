5b94a34f9e9250619594ced6339f8268e2e4c801
SaSoup-YR <yuruiwang77@gmail.com>
2026-08-22 01:55:53 +0100

    feat: install native RF-04 recovery dialog successor

--- source/src/main.ts
+++ source/src/main.ts
@@ -1,5 +1,7 @@
 import './styles.css';
 import './accessible-nasa-tlx';
 import { installRf04SavedSessionRecovery } from './rf04-saved-session-recovery';
+import { installRf04NativeRecoveryDialog } from './rf04-native-recovery-dialog';
 
 installRf04SavedSessionRecovery();
+installRf04NativeRecoveryDialog();
